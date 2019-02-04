e7f2afeb369f1d16fbfa0539c1580f4653a51dac
TOyefule <Tolani.Oyefule@gmail.com>
2019-02-04 17:15:37 -0500

    newcommit

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+//Challenge 2
+        public class Main {
+
+            public static void main(String[] args) {
+                Random r = new Random();
+                Scanner keyboard = new Scanner(System.in);
+
+                // Variables:
+                // int money = 0;
+                // int betAmount = 0;
+                int gamesWon = 0;
+                int gamesLost = 0;
+
+                //
+                int lowNumber = (int)(Math.random()%2);
+                int evenNumber =(int)(Math.random()%2);
+                int lowNumber = (int i=1; 1<=18; i++);
+                int highNumber=(int i=1; 19<=36; i++);
+
+
+
+
+                System.out.println(" WELCOME! Let's play some Roulette!");
+                System.out.println("Enter the type of bet you would like to place (red|black|even|odd|high|low): ");
+                System.in(keyboard);
+                do {
+                    if(i=1; i<=36;i++){ /*Do stuff*/};
+
+
+                }while(/*CONDITION */);
+
+                System.out.println("Enter in your bet amount: ");
+                System.in(35);
+                System.out.println(" The ball landed on " /*random.int(27) + random.color(black) + */".");
+                // User Prompt
+                if(state.col == true && state.num == true) {
+                    System.out.println("Congratulations, you've won.");
+                } else {
+                    System.out.println("Sorry, you've lost this bet.");
+
+                        // You currently have: $-35.0
+                    system.out.println("You currently have: $"money(-35.0));
+                    system.out.println("Would you like to play again (true|false)?");
+                    System.in(true);
+/*
+Enter the type of bet you would like to place (red|black|even|odd|high|low): low
+Enter in your bet amount: 40
+The ball landed on 17 black.
+Congratulations, you've won.
+You currently have: $45.0
+Would you like to play again (true|false)? true
+Enter the type of bet you would like to place (red|black|even|odd|high|low): even
+Enter in your bet amount: 100
+The ball landed on 29 black.
+Sorry, you've lost this bet.
+You currently have: $-55.0
+Would you like to play again (true|false)? false
+*/
+
+                  /*
+                     System.out.println("Thank you for playing!")
+                     System.out.println("This is how much money you have remaining: $" + money(-55.00))
+                     System.out.println("You've won " + gameswon +" game(s) and lost " +gameslost+ " game(s).")
+                      }
+
+                     */
+
+                  }
+                 }
+                }
